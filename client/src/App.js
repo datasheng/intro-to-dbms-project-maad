3c1be70a2d1f6d62156afcd366eacd59c0a4472d
Devin <dmunoz005@citymail.cuny.edu>
2024-07-21 12:57:53 -0400

    import client content

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+/**import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import Schedule from "./pages/Schedule";
+import UpdateCourse from "./pages/UpdateCourse";
+import "./style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update/:id" element={<Update/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+*/
