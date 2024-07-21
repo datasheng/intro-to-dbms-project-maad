9e1dde0d1b619bb4178d1fb66a65fa52e9f72661
gizmo1547 <120990499+gizmo1547@users.noreply.github.com>
2024-07-21 13:09:42 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,60 @@
+import express from "express" 
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express()
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"GermanVoronovich",
+    database:"enrollhub"
+})
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req,res)=>{
+    res.json("hello it's we are backend!")
+})
+
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+            return res.json(data)
+    })
+})
+
+
+app.post("/books", (req,res)=>{
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        //req.body.price,
+        req.body.cover,
+    ];
+
+    db.query(q,[values], (err, data)=>{
+        if(err) return res.json(err)
+            return res.json("Book has been created succesfuly.");
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log ("Connected to backend!")
+})
+
+
+
+
+
+/*
 import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
